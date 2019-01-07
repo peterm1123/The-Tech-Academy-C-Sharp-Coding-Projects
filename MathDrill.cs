7d230666e8e138ad9f71b5845b3b623735e2c273
peterm1123 <44681587+peterm1123@users.noreply.github.com>
2019-01-07 02:23:22 -0800

    Add files via upload

---
+++ MathDrill.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Mathdrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number: ");
+            string num1 = Console.ReadLine();
+            long number1 = Convert.ToInt64(num1);
+            long product = number1 * 50;
+            Console.WriteLine("Your number x 50 is " + product);
+
+            Console.WriteLine("Pick new number: ");
+            string num2 = Console.ReadLine();
+            long number2 = Convert.ToInt32(num2);
+            long total = number2 + 25;
+            Console.WriteLine("your number plus 25 is " + total);
+
+            Console.WriteLine("Pick another number: ");
+            string num3 = Console.ReadLine();
+            long number3 = Convert.ToInt64(num3);
+            decimal number4 = 12.5m;                      
+            decimal quotient = number3 / number4;        
+            Console.WriteLine("Your number divided by 12.5 is " + quotient);
+
+            Console.WriteLine("Pick one more number: ");
+            int num5 = int.Parse(Console.ReadLine());
+            if (num5 < 50)                              
+            {
+                Console.WriteLine("Your number is less than 50!");
+            }
+            else
+            {
+                Console.WriteLine("Your number is greater than 50!");
+            }
+
+
+            Console.WriteLine("Pick one last number for this: ");
+            string num6 = Console.ReadLine();
+            long number6 = Convert.ToInt32(num6);
+            long remainder = number6 % 7;
+            Console.WriteLine("When your number divided by 7, it has a remainder of " + remainder);
+            Console.ReadKey();
+        }
+    }
+}
