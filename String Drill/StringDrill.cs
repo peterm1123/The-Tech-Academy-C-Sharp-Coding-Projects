e199e9d5135f5268c479a752807675e2af43771d
peterm1123 <44681587+peterm1123@users.noreply.github.com>
2019-01-08 10:41:24 -0800

    Add files via upload

---
+++ String Drill/StringDrill.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+
+namespace StringDrill
+{
+    class StringDrill
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Good Afternoon. ");
+            Console.ReadLine();
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Okay " + name + ", Nice to meet you. What is your best strength?");
+            string userStrength = Console.ReadLine();
+            string strength = userStrength.ToUpper();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(". My best strength is " + strength + ". ");
+
+            Console.WriteLine("How will that help us out? ");
+
+            string fit = Console.ReadLine();
+            sb.Append("I think it will help you out because " + fit);
+
+            Console.WriteLine("Hello, my name is " + name + sb + ". Thank you for talking to me");
+            Console.ReadKey();
+
+
+        }
+    }
+}
