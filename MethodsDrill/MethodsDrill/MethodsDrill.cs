205f6490b20944337f2bc68b27edf480f3e74b8e
peterm1123 <44681587+peterm1123@users.noreply.github.com>
2019-01-09 16:28:08 -0800

    Add files via upload

---
+++ MethodsDrill/MethodsDrill/MethodsDrill.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+
+namespace MethodsDrill
+{
+    public class MethodsDrill
+    {
+        static void Main()
+        {
+            Console.WriteLine("Pick a number:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Addition a = new Addition();
+            int x = a.AddTwoValues(5, 10);
+            int y = a.AddThreeValues(15, 25, 35);
+            int z = a.AddFourValues(10, 20, 30, 40);
+
+            Console.WriteLine(x + userInput);
+            Console.WriteLine(y + userInput);
+            Console.WriteLine(z + userInput);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodsDrill/MethodsDrill/MethodsDrillParameters.cs
@@ -0,0 +1,21 @@
+﻿using MethodsDrill;
+
+
+namespace MethodsDrill
+{
+    public class Addition
+    {
+        public int AddTwoValues(int x, int y)
+        {
+            return x + y;
+        }
+        public int AddThreeValues(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+        public int AddFourValues(int x, int y, int z, int i)
+        {
+            return x + y + z + i;
+        }
+    }
+}
