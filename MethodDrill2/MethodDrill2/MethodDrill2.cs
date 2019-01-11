4060a713f3c73d558c9295263aed13afd64d0fde
peterm1123 <44681587+peterm1123@users.noreply.github.com>
2019-01-10 16:22:47 -0800

    Add files via upload

---
+++ MethodDrill2/MethodDrill2/MethodDrill2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MethodDrill2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            int result = 0;
+            result = calc.Multiply(12, 10);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            Calculator calc2 = new Calculator();
+            int result2 = 0;
+            result2 = calc2.Subtract(15, 2.2);
+            Console.WriteLine(result2);
+            Console.ReadLine();
+
+            Calculator calc3 = new Calculator();
+
+            int result3 = 0;
+            result3 = calc3.Add("15", "5");
+            Console.WriteLine(result3);
+            Console.ReadLine();
+
+        }
+    }
+}
