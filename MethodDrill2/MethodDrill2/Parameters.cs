4073416102f24afdf3765e420c6f4d9a685559f1
peterm1123 <44681587+peterm1123@users.noreply.github.com>
2019-01-10 16:15:22 -0800

    Add files via upload

---
+++ MethodDrill2/MethodDrill2/Parameters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MethodDrill2
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Subtract(double num1, double num2)
+        {
+            return Convert.ToInt32(num1 - num2);
+        }
+
+        public int Add(string one, string two)
+        {
+            int x = 0;
+            int y = 0;
+            if (!Int32.TryParse(one, out x))
+            {
+                x = -1;
+            }
+            if (!Int32.TryParse(two, out y))
+            {
+                y = -1;
+            }
+            return x + y;
+        }
+    }
+}
