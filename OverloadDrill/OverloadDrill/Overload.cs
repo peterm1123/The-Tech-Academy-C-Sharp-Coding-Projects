1e46b8ff1d2d0cffa72bba90a5a4a2a177f284b2
peterm1123 <44681587+peterm1123@users.noreply.github.com>
2019-01-10 22:17:15 -0800

    overload drill

---
+++ OverloadDrill/OverloadDrill/Overload.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace OverloadDrill
+{
+    public static class Program
+    {
+
+        static void Main()
+        {
+            int x = 0;
+            int y = 0;
+            Method_Parameters method = new Method_Parameters();
+            Console.WriteLine("Please enter an integer");
+            x = Convert.ToInt32(Console.ReadLine());
+            method.Example1(out y, x);
+            Console.WriteLine("The first answer: " + x + " divided by 2 equals " + y);
+            Console.WriteLine("Please hit enter to overload a function");
+            Console.ReadLine();
+
+            method.Overload(10, 20);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ OverloadDrill/OverloadDrill/parameters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace OverloadDrill
+
+{
+    public class Method_Parameters
+    {
+
+        public void Example1(out int y, int x)
+        {
+            y = x / 2;
+        }
+
+        public void Overload(int x, int y)
+        {
+            Console.WriteLine("Overload(int x, int y)");
+        }
+
+        public void Overload(int x, double y)
+        {
+            Console.WriteLine("Overload(int x, double y)");
+        }
+
+        public void Overload(double x, int y)
+        {
+            Console.WriteLine("Overload(double x, int y)");
+        }
+
+    }
+}
