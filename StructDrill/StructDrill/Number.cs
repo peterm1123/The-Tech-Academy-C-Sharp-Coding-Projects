947bd70114672c26f77ccdf78025b968774b2338
peterm1123 <44681587+peterm1123@users.noreply.github.com>
2019-01-11 03:21:49 -0800

    Add files via upload

---
+++ StructDrill/StructDrill/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StructDrill
+{
+    public struct Number
+    {
+        public decimal x;
+
+        public Number(decimal am1)
+        {
+            x = am1;
+        }
+
+    }
+}
---
+++ StructDrill/StructDrill/StructDrill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StructDrill
+{
+
+    public class StructDrill
+    {
+        static void Main()
+        {
+            Number amount1 = new Number(15m);
+
+            Console.WriteLine(amount1.x);
+            Console.ReadLine();
+        }
+    }
+}
