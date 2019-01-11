51a6f2c7fc1fa6141426a5b442be3c7f2ee6a496
peterm1123 <44681587+peterm1123@users.noreply.github.com>
2019-01-11 02:44:22 -0800

    Add files via upload

---
+++ EnumDrill/EnumDrill/Days.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EnumDrill
+{
+    enum Day { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
+
+    public class Days
+    {
+
+        public enum Day
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+    }
+}
---
+++ EnumDrill/EnumDrill/EnumDrill.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EnumDrill
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter what Day of the week it is.");
+            string dayValue = Console.ReadLine();
+
+            try
+            {
+                Day day = (Day)Enum.Parse(typeof(Day), dayValue);
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day it is.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
