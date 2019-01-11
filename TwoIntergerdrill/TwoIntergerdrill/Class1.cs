efdd427210528aab059a88c9688fb97886d75d9d
peterm1123 <44681587+peterm1123@users.noreply.github.com>
2019-01-10 21:41:53 -0800

    Add files via upload

---
+++ TwoIntergerdrill/TwoIntergerdrill/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TwoIntergerdrill
+
+    {
+        public class Example
+        {
+
+            public void AddNumbers(int a, [Optional] int[] b)
+            {
+                int total = a + 10;
+                if (b != null)
+                {
+                    foreach (int i in b)
+                    {
+                        total += i;
+                    }
+                }
+                Console.WriteLine("10 + integer one + integer two:");
+                Console.WriteLine(total);
+
+            }
+
+        }
+    }
---
+++ TwoIntergerdrill/TwoIntergerdrill/TwoIntergerdrill.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace TwoIntergerdrill
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine("Please enter an integer.");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter a second integer (it is not necessary).");
+            int y = 0;
+            try
+            {
+                y = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Nothing was entered for second integer");
+            }
+
+
+            Example add = new Example();
+            add.AddNumbers(x, new int[] { y });
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
