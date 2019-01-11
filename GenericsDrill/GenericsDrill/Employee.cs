5219ec5710c89a24129543389d88cdcb779a9ed2
peterm1123 <44681587+peterm1123@users.noreply.github.com>
2019-01-11 02:13:41 -0800

    Add files via upload

---
+++ GenericsDrill/GenericsDrill/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsDrill
+{
+
+    public class Employee<T>
+    {
+
+        public List<T> Things { get; set; }
+
+        public string Items { get; set; }
+
+        public int Numbers { get; set; }
+
+    }
+
+
+
+
+}
---
+++ GenericsDrill/GenericsDrill/GenericsDrilll.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsDrill
+{
+    class GenericsDrilll
+    {
+
+        static void Main(string[] args)
+        {
+            Employee<string> employee1 = new Employee<string>();
+            List<string> Items = new List<string>();
+            Items.Add("Book");
+            Items.Add("Pencil");
+            Items.Add("Watch");
+            Items.Add("Phone");
+            Items.Add("Glasses");
+
+            Employee<int> employee2 = new Employee<int>();
+            List<int> Numbers = new List<int>();
+            Numbers.Add(2);
+            Numbers.Add(7);
+            Numbers.Add(5);
+            Numbers.Add(1);
+            Numbers.Add(3);
+
+            foreach (string item in Items)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (int i in Numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
