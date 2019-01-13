457f4943a7ed55d41fabf5a3afc14368f6653731
peterm1123 <44681587+peterm1123@users.noreply.github.com>
2019-01-12 17:05:25 -0800

    Add files via upload

---
+++ DateTimeDrill/DateTimeDrill/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace DateTimeDrill
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("The current Date and Time is: ");
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("Please enter a number.");
+            int X = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("In {0} hours, it will be ", X);
+            double[] hours = { X };
+            DateTime dateValue = DateTime.Now;
+
+            foreach (double hour in hours)
+                Console.WriteLine("{0} + {1} hour(s) = {2}", dateValue, hour, dateValue.AddHours(hour));
+            Console.ReadLine();
+        }
+    }
+}
