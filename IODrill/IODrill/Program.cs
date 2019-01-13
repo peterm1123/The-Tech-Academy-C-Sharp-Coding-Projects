542b22535c93e5d8256f885560cfafc2e6d369bc
peterm1123 <44681587+peterm1123@users.noreply.github.com>
2019-01-12 16:35:25 -0800

    Add files via upload

---
+++ IODrill/IODrill/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace IODrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Test text\nMore text";
+            Console.WriteLine("Please enter a number to save to file");
+            string num = Console.ReadLine();
+            string filePath = @"C:\Users\Nashi\log.txt";
+            File.WriteAllText(filePath, text + "\n" + num);
+
+            string readText = File.ReadAllText(filePath);
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Contents of text file:\n");
+            Console.WriteLine(readText);
+        }
+    }
+}
