d3dda0429b040bf991857e9dbe6f084973d8b67d
peterm1123 <44681587+peterm1123@users.noreply.github.com>
2019-01-13 16:22:50 -0800

    Add files via upload

---
+++ ExceptionHandlingDrill2/ExceptionHandlingDrill2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace ExceptionHandlingDrill2
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Please enter your age.");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                int birthYear = 2018 - userInput;
+                if (userInput < 1)
+                {
+                    throw new ArgumentException();
+                }
+                Console.WriteLine(birthYear);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter a real age.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.ReadLine();
+        }
+    }
+}
