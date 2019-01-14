22d3608ebe6a1cce582ace75b9eb86cc250e7b1a
peterm1123 <44681587+peterm1123@users.noreply.github.com>
2019-01-13 16:20:41 -0800

    Add files via upload

---
+++ ConstantVarDrill/ConstantVarDrill/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace ConstantVarDrill
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Student student = new Student();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConstantVarDrill/ConstantVarDrill/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConstantVarDrill
+{
+    public class Student
+    {
+        public Student() : this("", "")
+        {
+        }
+
+        public Student(string name, string city)
+        {
+            const string studentName = "Wade Wilson";
+            var City = "Spokane";
+            Console.WriteLine("Student name = {0}, City = {1}", studentName, City);
+        }
+    }
+}
